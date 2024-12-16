b89dae4bf9e8fd0d55e8d66e5114ca062624fa6f
Jérôme DUBOS <jeromedubos67@gmail.com>
2024-12-16 13:38:19 +0100

    Ajout message confirmation

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -4,7 +4,10 @@ import Field, { FIELD_TYPES } from "../../components/Field/Field";
 import Select from "../../components/Select/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); })
+const mockContactApi = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 500);
+  });
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
@@ -13,9 +16,11 @@ const Form = ({ onSuccess, onError }) => {
       evt.preventDefault();
       setSending(true);
       // We try to call mockContactApi
+
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess(); // Appel de fontion en cas de succès
       } catch (err) {
         setSending(false);
         onError(err);
@@ -23,6 +28,7 @@ const Form = ({ onSuccess, onError }) => {
     },
     [onSuccess, onError]
   );
+
   return (
     <form onSubmit={sendContact}>
       <div className="row">
@@ -56,11 +62,11 @@ const Form = ({ onSuccess, onError }) => {
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-}
+};
 
 export default Form;
